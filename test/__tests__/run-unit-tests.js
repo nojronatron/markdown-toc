f7e59a9df435a4ffb5f0fc96c50b61946b0121fb
Jon Rumsey <jon.rumsey@gmail.com>
2023-06-12 16:45:11 -0700

    implemeneted tests, removed old.

--- test/__tests__/run-unit-tests.js
+++ test/__tests__/run-unit-tests.js
@@ -1,8 +1,23 @@
 const { expect } = require('@jest/globals');
-const starterFunction = require('../../extension-functions/starter-function');
+const path = require('path');
 
-describe('starterFunction', () => {
-  test('should return true', () => {
-    expect(starterFunction()).toBe(true);
+const functionsRoot = path.resolve(__dirname, '../../extension-functions');
+const createTOC = require(path.resolve(functionsRoot, 'create-toc.js'));
+
+describe('unit test extension functions', () => {
+  let helloWorld =
+    '# First Heading\n\n## Second Heading\n\n## Third Heading\n\n### Bad Heading\n';
+  let helloWorldToc =
+    '## Table of Contents\n\n- [Second Heading](#second-heading)\n- [Third Heading](#third-heading)\n';
+  let noHeadings =
+    'First Heading\n\nSecond Heading\n\nThird Heading\n\nBad Heading\n';
+  let noHeadingsToc = '';
+
+  test('Creates TOC with valid ## headings', () => {
+    expect(createTOC(helloWorld)).toMatch(helloWorldToc);
+  });
+
+  test('Does NOT create TOC without ## headings', () => {
+    expect(createTOC(noHeadings)).toMatch(noHeadingsToc);
   });
 });
--- test/suite/extension.test.js
+++ test/suite/extension.test.js
@@ -1,5 +1,4 @@
 const assert = require('assert');
-const createTOC = require('../extension-functions/create-toc');
 
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
@@ -9,17 +8,8 @@ const vscode = require('vscode');
 suite('Extension Test Suite', () => {
   vscode.window.showInformationMessage('Start all tests.');
 
-  let helloWorld =
-    '# First Heading\n\n## Second Heading\n\n## Third Heading\n\n### Bad Heading\n';
-  let helloWorldToc =
-    '# First Heading\n\n## Table of Contents\n\n- [Second Heading](#second-heading)\n- [Third Heading](#third-heading)\n\n## Second Heading\n\n## Third Heading\n\n### Bad Heading\n';
-
-  let noHeadings =
-    'First Heading\n\nSecond Heading\n\nThird Heading\n\nBad Heading\n';
-  let noHeadingsToc = '';
-
-  test('Create TOC', () => {
-    assert.strictEqual(createTOC(helloWorld), helloWorldToc);
+  test('Sample test', () => {
+    assert.strictEqual(-1, [1, 2, 3].indexOf(5));
     assert.strictEqual(-1, [1, 2, 3].indexOf(0));
   });
 });
