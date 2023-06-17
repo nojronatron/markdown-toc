ac64cbda9ea4c328316ddb1effafa042d9171487
Jon Rumsey <jon.rumsey@gmail.com>
2023-06-16 20:27:50 -0700

    refactored args to type TextDocument

--- extension-functions/find-top-heading.js
+++ extension-functions/find-top-heading.js
@@ -1,12 +1,12 @@
 /**
  * Method to find the top heading of the page.
+ * @param {vscode.TextEditor.document} document The active text editor.
  * @returns Number The line number of the top 1st Level Heading of the page.
- * @param {vscode.TextEditor} editor The active text editor.
  */
-module.exports = function findTopHeading(editor) {
+module.exports = function findTopHeading(document) {
   let topHeading = 0;
-  for (let i = 0; i < editor.document.lineCount; i++) {
-    const line = editor.document.lineAt(i);
+  for (let i = 0; i < document.lineCount; i++) {
+    const line = document.lineAt(i);
     if (line.text.startsWith('# ')) {
       topHeading = i;
       break;
