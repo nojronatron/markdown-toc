ffd8e1b4efc781081480769c8a109e71b167a469
Jon Rumsey <jonrumsey.dev@gmail.com>
2024-06-30 20:53:34 -0700

    refactor extension.js to handle hash and dash

--- extension.js
+++ extension.js
@@ -1,6 +1,6 @@
 const vscode = require('vscode');
 const findTopHeading = require('./extension-functions/find-top-heading');
-const findFirstSecondLevelHeading = require('./extension-functions/find-first-second-level-heading');
+const getSecondLevelHeading = require('./extension-functions/get-second-level-heading');
 const createTOC = require('./extension-functions/create-toc');
 
 /**
@@ -16,49 +16,64 @@ function activate(context) {
       const editor = vscode.window.activeTextEditor;
 
       if (editor && editor.document.languageId === 'markdown') {
-        // 1. Find the top heading of the page.
+        // 1. Find the top heading of the page and store in a plain 
+        // obj with parameters Text, Line Number, and Hash true/false.
         let topHeading = findTopHeading(editor.document);
-
-        // 2. Find the first occurrence of a second level heading in the document. If there is no second level heading, then the TOC is not needed.
-        let firstSecondLevelHeading = findFirstSecondLevelHeading(
-          editor.document,
-          topHeading
-        );
-
-        // avoid overwriting or including an existing table of contents.
-        if (firstSecondLevelHeading === -1) {
-          vscode.window.showWarningMessage('TOC already exists.');
-          return null;
-        }
-
-        // avoid making a table of contents if there are no second level headings.
-        if (firstSecondLevelHeading === 0) {
+        if (topHeading.line < 0) {
           vscode.window.showWarningMessage(
-            'No second level heading found. TOC not created.'
+            'No top level headings found. Will put ToC at top of document.'
           );
+        }
+        
+        // Check if there is enough content to add a TOC
+        if (editor.document.lineCount <= topHeading.line + 3) {
+          vscode.window.showWarningMessage(
+            'Add more content below first level and try again.'
+          );
+          
           return null;
         }
 
-        // 3. Create the TOC as a string.
-        const capturedDocument = editor.document.getText(
-          new vscode.Range(
-            firstSecondLevelHeading,
-            firstCharacter,
-            editor.document.lineCount,
-            firstCharacter
-          )
-        );
+        // 2. determine whether to use hash or dash for headings style
+        // This code assumes markdown is consistent with one or the other
+        let secondLevelHeadingChar = topHeading.isHash ? '##' : '-';
+        let useHashCharacter = topHeading.isHash;
 
-        let tableOfContents = createTOC(capturedDocument);
+        // 3. iterate over the document to find and store all second level headings
+        const secondLevelHeadings = [];
+        let start = topHeading.line + 3;
+        for (let idx = start; idx < editor.document.lineCount; idx++) {
+          let firstLine = editor.document.lineAt(idx-1).text;
+          let secondLine = editor.document.lineAt(idx).text;
+          const headingObject = getSecondLevelHeading(firstLine, idx-1, secondLine, useHashCharacter);
+          if (headingObject.line !== -1) {
+            secondLevelHeadings.push(headingObject);
+          }
+        }
 
-        // 4. Insert the TOC at the top of the document.
+        // no second level headings? no table of contents to create
+        if (secondLevelHeadings.length < 1) {
+          vscode.window.showWarningMessage(
+            'No second level headings to reference.'
+          );
+
+          return null;
+        }
+
+        // 4. Create the TOC as a string.
+        let tableOfContents = createTOC(secondLevelHeadings);
+
+        // 5. Insert the TOC after top level1 content and before first existing H2 block
         const edit = new vscode.WorkspaceEdit();
         edit.insert(
           editor.document.uri,
-          new vscode.Position(firstSecondLevelHeading, firstCharacter),
+          new vscode.Position(secondLevelHeadings[0].line, firstCharacter),
           tableOfContents
         );
+
+        // apply the changes
         await vscode.workspace.applyEdit(edit);
+        
         vscode.window.showInformationMessage('TOC created.');
       } else {
         vscode.window.showWarningMessage('No Markdown file is currently open.');
