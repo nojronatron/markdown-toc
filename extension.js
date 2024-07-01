b03a7bf26a9078411d782d7572d9369a74e2586e
Jon Rumsey <jonrumsey.dev@gmail.com>
2024-07-01 13:51:05 -0700

    refactor, debug extension.js

--- extension.js
+++ extension.js
@@ -1,10 +1,12 @@
 const vscode = require('vscode');
 const findTopHeading = require('./extension-functions/find-top-heading');
-const getSecondLevelHeading = require('./extension-functions/get-second-level-heading');
+const {getSecondLevelHeading, findExistingStyleCharacter} = require('./extension-functions/get-second-level-heading');
 const createTOC = require('./extension-functions/create-toc');
 
 /**
- * @param {vscode.ExtensionContext} context
+ * Activates the extension and executes custom extension code in a child async function.
+ * @param {vscode.ExtensionContext} context 
+ * @returns {void}
  */
 function activate(context) {
   console.log('Markdown TOC is now active!');
@@ -14,17 +16,36 @@ function activate(context) {
     async function () {
       const firstCharacter = 0;
       const editor = vscode.window.activeTextEditor;
+      let topHeading = { line: -1, text: '', isHash: true, isToc: false };
 
       if (editor && editor.document.languageId === 'markdown') {
         // 1. Find the top heading of the page and store in a plain 
         // obj with parameters Text, Line Number, and Hash true/false.
-        let topHeading = findTopHeading(editor.document);
+        topHeading = findTopHeading(editor.document.getText());
+
         if (topHeading.line < 0) {
           vscode.window.showWarningMessage(
-            'No top level headings found. Will put ToC at top of document.'
+            'No top level headings found.'
           );
+
+          // 2. determine whether to use hash or dash for headings style
+          let foundCharacter = findExistingStyleCharacter(editor.document.getText());
+          
+          switch (foundCharacter) {
+            case '#':
+              topHeading.isHash = true;
+              break;
+            case '-':
+              topHeading.isHash = false;
+              break;
+            default:
+              vscode.window.showWarningMessage(
+                'No heading style found. Add a heading and try again.'
+              );
+              return null;
+          }
         }
-        
+
         // Check if there is enough content to add a TOC
         if (editor.document.lineCount <= topHeading.line + 3) {
           vscode.window.showWarningMessage(
@@ -34,19 +55,22 @@ function activate(context) {
           return null;
         }
 
-        // 2. determine whether to use hash or dash for headings style
-        // This code assumes markdown is consistent with one or the other
-        let secondLevelHeadingChar = topHeading.isHash ? '##' : '-';
-        let useHashCharacter = topHeading.isHash;
-
         // 3. iterate over the document to find and store all second level headings
         const secondLevelHeadings = [];
-        let start = topHeading.line + 3;
+        let start = topHeading.line > -1 ? topHeading.line + 3 : 1;
         for (let idx = start; idx < editor.document.lineCount; idx++) {
           let firstLine = editor.document.lineAt(idx-1).text;
           let secondLine = editor.document.lineAt(idx).text;
-          const headingObject = getSecondLevelHeading(firstLine, idx-1, secondLine, useHashCharacter);
+          const headingObject = getSecondLevelHeading(firstLine, idx-1, secondLine, topHeading.isHash);
+
+          if (headingObject.isToc) {
+            // do no harm to existing document and allow user to make changes
+            vscode.window.showWarningMessage('Table of Contents already exists.');
+            return null;
+          }
+
           if (headingObject.line !== -1) {
+            // add the heading object to the array
             secondLevelHeadings.push(headingObject);
           }
         }
@@ -56,7 +80,6 @@ function activate(context) {
           vscode.window.showWarningMessage(
             'No second level headings to reference.'
           );
-
           return null;
         }
 
