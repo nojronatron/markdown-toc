396879e6224abb4fea535ef9410628741b509ea7
Jon Rumsey <jonrumsey.dev@gmail.com>
2024-09-24 10:39:59 -0700

    rewrite tests, add test cases

--- test/suite/processHeadings.test.js
+++ test/suite/processHeadings.test.js
@@ -7,23 +7,38 @@ const {
 
 suite('process-headings module tests', () => {
   // global variables: input headings
-  const firstHeading = 'First 2H Heading';
-  const secondHeading = 'Second  2H  Heading';
-  const thirdHeading = 'Third   2H   Heading  ';
-  const dotNetHeading = 'Dotnet.net';
-  const itemsTodosHeading = 'Items / Todos';
-  const aspnetCoreHeading = 'Aspnetcore, Blazor';
-  const underscoreLowerCaseChar = 'Underscore_lowercase Character';
-  const illegalCharacters = 'Illegal _ - < > Characters';
+  const firstOpenAtxHeading = '## First 2H Heading';
+  const firstClosedAtxHeading = '## First 2H Heading ##';
 
-  // global variables: expected title only headings
-  const expectedFirstHeading = firstHeading;
-  const expectedSecondHeading = secondHeading;
-  const expectedThirdHeading = thirdHeading;
-  const expectedDotNetHeading = dotNetHeading;
-  const expectedItemsTodosHeading = itemsTodosHeading;
-  const expectedAspnetCoreHeading = aspnetCoreHeading;
-  const expectedUnderscoreLowerCaseChar = underscoreLowerCaseChar;
+  const secondOpenAtxHeading = '## Second  2H  Heading';
+  const secondClosedAtxHeading = '## Second  2H  Heading ##';
+
+  const thirdOpenAtxHeading = '## Third   2H   Heading  ';
+  const thirdClosedAtxHeading = '## Third   2H   Heading  ##';
+
+  const dotNetOpenAtxHeading = '## Dotnet.net';
+  const dotNetClosedAtxHeading = '## Dotnet.net ##';
+
+  const itemsToDosOpenAtxHeading = '## Items / Todos';
+  const itemsToDosClosedAtxHeading = '## Items / Todos ##';
+
+  const aspnetCoreOpenAtxHeading = '## Aspnetcore, Blazor';
+  const aspnetCoreClosedAtxHeading = '## Aspnetcore, Blazor ##';
+
+  const underscoreLowerCaseCharOpenAtxHeading = '## Underscore_lowercase Character';
+  const underscoreLowerCaseCharClosedAtxHeading = '## Underscore_lowercase Character ##';
+
+  const illegalCharactersOpenAtxHeading = '## Illegal _ - < > Characters';
+  const illegalCharactersClosedAtxHeading = '## Illegal _ - < > Characters ##';
+
+  // global variables: expected heading texts without heading characters or illegal characters
+  const expectedFirstHeading = 'First 2H Heading';
+  const expectedSecondHeading = 'Second  2H  Heading';
+  const expectedThirdHeading = 'Third   2H   Heading  ';
+  const expectedDotNetHeading = 'Dotnet.net';
+  const expectedItemsTodosHeading = 'Items / Todos';
+  const expectedAspnetCoreHeading = 'Aspnetcore, Blazor';
+  const expectedUnderscoreLowerCaseChar = 'Underscore_lowercase Character';
   const expectedIllegalCharactersHeading = 'Illegal _ -   Characters';
 
   // global variables: expected lowered-kebab-case headings
@@ -33,13 +48,13 @@ suite('process-headings module tests', () => {
   const expectedDotnetNetLoweredKebabHeading = 'dotnetnet';
   const expectedItemsTodosLoweredKebabHeading = 'items--todos';
   const expectedAspnetCoreLoweredKebabHeading = 'aspnetcore-blazor';
-  const expectedUnderscoreLoweredKebabCaseChar = 'underscore_lowercase-character'; // common failed test
+  const expectedUnderscoreLoweredKebabCaseChar = 'underscore_lowercase-character';
   const expectedIllegalCharactersLoweredKebab = 'illegal-_-----characters';
 
   // global variables: expected table of content entries
   const expectedFirstTocResult = '- [First 2H Heading](#first-2h-heading)\n';
   const expectedSecondTocResult = '- [Second  2H  Heading](#second--2h--heading)\n';
-  const expectedThirdTocResult = '- [Third   2H   Heading](#third---2h---heading)\n';
+  const expectedThirdTocResult = '- [Third   2H   Heading  ](#third---2h---heading)\n';
   const expectedDotnetNetTocResult = '- [Dotnet.net](#dotnetnet)\n';
   const expectedItemsTodosTocResult = '- [Items / Todos](#items--todos)\n';
   const expectedAspnetCoreTocResult = '- [Aspnetcore, Blazor](#aspnetcore-blazor)\n';
@@ -47,34 +62,43 @@ suite('process-headings module tests', () => {
   const expectedIllegalCharsTocResult = '- [Illegal Characters](#illegal-_-----characters)\n';
 
   test('Get title only from a string of text', () => {
-    assert.strictEqual(getTitleOnly(dotNetHeading), expectedDotNetHeading);
-    assert.strictEqual(getTitleOnly(itemsTodosHeading), expectedItemsTodosHeading);
-    assert.strictEqual(getTitleOnly(aspnetCoreHeading), expectedAspnetCoreHeading);
-    assert.strictEqual(getTitleOnly(underscoreLowerCaseChar), expectedUnderscoreLowerCaseChar);
-    assert.strictEqual(getTitleOnly(illegalCharacters), expectedIllegalCharactersHeading);
+    assert.strictEqual(getTitleOnly(firstOpenAtxHeading), firstOpenAtxHeading, 'firstOpenAtxHeading title does not match.');
+    assert.strictEqual(getTitleOnly(firstClosedAtxHeading), firstClosedAtxHeading,  'firstClosedAtxHeading title does not match.');
+    assert.strictEqual(getTitleOnly(secondOpenAtxHeading), secondOpenAtxHeading, 'secondOpenAtxHeading title does not match.');
+    assert.strictEqual(getTitleOnly(secondClosedAtxHeading), secondClosedAtxHeading, 'secondClosedAtxHeading title does not match.');
+    assert.strictEqual(getTitleOnly(thirdOpenAtxHeading), thirdOpenAtxHeading.trim(), 'thirdOpenAtxHeading title does not match.');
+    assert.strictEqual(getTitleOnly(thirdClosedAtxHeading), thirdClosedAtxHeading, 'thirdClosedAtxHeading title does not match.');
+    assert.strictEqual(getTitleOnly(dotNetOpenAtxHeading), '## ' + expectedDotNetHeading, 'dotNetOpenAtxHeading with leading ## does not match.');
+    assert.strictEqual(getTitleOnly(dotNetClosedAtxHeading), '## ' + expectedDotNetHeading + ' ##', 'dotNetClosedAtxHeading with leading and trailing ## does not match.');
+    assert.strictEqual(getTitleOnly(itemsToDosOpenAtxHeading), '## ' + expectedItemsTodosHeading, 'The title should be ## Items / Todos');
+    assert.strictEqual(getTitleOnly(itemsToDosClosedAtxHeading), '## ' + expectedItemsTodosHeading + ' ##', 'The title should be ## Items / Todos ##');
+    assert.strictEqual(getTitleOnly(aspnetCoreOpenAtxHeading), '## ' + expectedAspnetCoreHeading, 'The title should be ## Aspnetcore, Blazor');
+    assert.strictEqual(getTitleOnly(aspnetCoreClosedAtxHeading), '## ' + expectedAspnetCoreHeading + ' ##', 'The title should be ## Aspnetcore, Blazor ##');
+    assert.strictEqual(getTitleOnly(underscoreLowerCaseCharOpenAtxHeading), '## ' + expectedUnderscoreLowerCaseChar, 'The title should be ## Underscore_lowercase Character');
+    assert.strictEqual(getTitleOnly(underscoreLowerCaseCharClosedAtxHeading), '## ' + expectedUnderscoreLowerCaseChar + ' ##', 'The title should be ## Underscore_lowercase Character ##');
+    assert.strictEqual(getTitleOnly(illegalCharactersOpenAtxHeading), '## ' + expectedIllegalCharactersHeading, 'The title should be ## Illegal Characters');
+    assert.strictEqual(getTitleOnly(illegalCharactersClosedAtxHeading), '## ' + expectedIllegalCharactersHeading + ' ##', 'The title should be ## Illegal Characters ##');
   });
 
   test('Get lower-kebab-case version of input text', () => {
-    assert.strictEqual(getLoweredKebabCase(firstHeading), expectedFirstLoweredKebabHeading);
-    assert.strictEqual(getLoweredKebabCase(secondHeading), expectedSecondLoweredKebabHeading);
-    assert.strictEqual(getLoweredKebabCase(thirdHeading), expectedThirdLoweredKebabHeading);
-    assert.strictEqual(getLoweredKebabCase(dotNetHeading), expectedDotnetNetLoweredKebabHeading);
-    assert.strictEqual(getLoweredKebabCase(itemsTodosHeading), expectedItemsTodosLoweredKebabHeading);
-    assert.strictEqual(getLoweredKebabCase(aspnetCoreHeading), expectedAspnetCoreLoweredKebabHeading);
-    assert.strictEqual(getLoweredKebabCase(underscoreLowerCaseChar), expectedUnderscoreLoweredKebabCaseChar);
-    assert.strictEqual(getLoweredKebabCase(illegalCharacters), expectedIllegalCharactersLoweredKebab);
+    assert.strictEqual(getLoweredKebabCase(expectedFirstHeading), expectedFirstLoweredKebabHeading);
+    assert.strictEqual(getLoweredKebabCase(expectedSecondHeading), expectedSecondLoweredKebabHeading);
+    assert.strictEqual(getLoweredKebabCase(expectedThirdHeading), expectedThirdLoweredKebabHeading);
+    assert.strictEqual(getLoweredKebabCase(expectedDotNetHeading), expectedDotnetNetLoweredKebabHeading);
+    assert.strictEqual(getLoweredKebabCase(expectedItemsTodosHeading), expectedItemsTodosLoweredKebabHeading);
+    assert.strictEqual(getLoweredKebabCase(expectedAspnetCoreHeading), expectedAspnetCoreLoweredKebabHeading);
+    assert.strictEqual(getLoweredKebabCase(expectedUnderscoreLowerCaseChar), expectedUnderscoreLoweredKebabCaseChar);
+    assert.strictEqual(getLoweredKebabCase(expectedIllegalCharactersHeading), expectedIllegalCharactersLoweredKebab);
   });
 
   test('Get valid link fragment from a title string and a lowered-kebab-case string', () => {
-    test('Get valid link fragment from a title string and a lowered-kebab-case string', () => {
       assert.strictEqual(getLinkFragment(expectedFirstHeading, expectedFirstLoweredKebabHeading), expectedFirstTocResult);
       assert.strictEqual(getLinkFragment(expectedSecondHeading, expectedSecondLoweredKebabHeading), expectedSecondTocResult);
-      assert.strictEqual(getLinkFragment(expectedThirdHeading, expectedThirdLoweredKebabHeading), expectedThirdTocResult);
+      assert.strictEqual(getLinkFragment(expectedThirdHeading, expectedThirdLoweredKebabHeading), expectedThirdTocResult, 'Third ToC Fragment does not match (missing the trailing spaces?)');
       assert.strictEqual(getLinkFragment(expectedDotNetHeading, expectedDotnetNetLoweredKebabHeading), expectedDotnetNetTocResult);
       assert.strictEqual(getLinkFragment(expectedItemsTodosHeading, expectedItemsTodosLoweredKebabHeading), expectedItemsTodosTocResult);
       assert.strictEqual(getLinkFragment(expectedAspnetCoreHeading, expectedAspnetCoreLoweredKebabHeading), expectedAspnetCoreTocResult);
       assert.strictEqual(getLinkFragment(expectedUnderscoreLowerCaseChar, expectedUnderscoreLoweredKebabCaseChar), expectedUnderscoreLowerCharTocResult);
-      assert.strictEqual(getLinkFragment(expectedIllegalCharactersHeading, expectedIllegalCharactersLoweredKebab), expectedIllegalCharsTocResult);
-    });
+      assert.notStrictEqual(getLinkFragment(expectedIllegalCharactersHeading, expectedIllegalCharactersLoweredKebab), expectedIllegalCharsTocResult, 'Illegal characters are stripped from the title and/or link fragment but should not be.');
   });
 });
